8c3fdff2d1c1608d3ccb7c96b89c4fb578e9e66d
Husain Zaveri <husain53zaveri30@gmail.com>
2025-05-12 18:26:33 +0530

    defined all API paths in apiPath.js in frontend

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -13,11 +13,6 @@ const getDashboardData = async (req, res) => {
       { $group: { _id: null, total: { $sum: "$amount" } } },
     ]);
 
-    console.log("Total Income", {
-      totalIncome,
-      userId: isValidObjectId(userId),
-    });
-
     const totalExpense = await Expense.aggregate([
       { $match: { userId: userObjectId } },
       { $group: { _id: null, total: { $sum: "$amount" } } },
