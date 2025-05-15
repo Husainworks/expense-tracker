1c362112ebadad851eb40f9192a24bafaa253867
Husain Zaveri <husain53zaveri30@gmail.com>
2025-05-15 10:11:16 +0530

    designed profile page

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 import { SideMenu } from "../SideMenu";
+import { Link } from "react-router";
+import { UserContext } from "../../context/UserContext";
+import CharAvatar from "../Cards/CharAvatar";
+import { getInitials } from "../../utils/helper";
 
 export const Navbar = ({ activeMenu }) => {
   const [openSideMenu, setOpenSideMenu] = useState(false);
+  const { user, clearUser } = useContext(UserContext);
 
   return (
     <>
@@ -21,7 +26,29 @@ export const Navbar = ({ activeMenu }) => {
           )}
         </button>
 
-        <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-lg font-medium text-black">
+            <Link to="/dashboard">Expense Tracker</Link>
+          </h2>
+
+          <Link to="/profile">
+            {user?.profileImgURL ? (
+              <img
+                src={user?.profileImgURL || ""}
+                alt="Profile Image"
+                className="w-15 h-15 bg-slate-400 rounded-full"
+              />
+            ) : (
+              <>
+                <div
+                  className={`w-12 h-12 flex items-center justify-center rounded-full text-gray-900 font-medium bg-gray-100`}
+                >
+                  {getInitials(user?.fullName || "")}
+                </div>
+              </>
+            )}
+          </Link>
+        </div>
 
         {openSideMenu && (
           <div className="fixed top-[61px] -ml-4 bg-white">
--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,12 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { InfoCard } from "../../components/Cards/InfoCard";
+import { IoMdCard } from "react-icons/io";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { RxUpdate } from "react-icons/rx";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { RecentIncome } from "../../components/Dashboard/RecentIncome";
+import { ExpenseTransactions } from "../../components/Dashboard/ExpenseTransactions";
 
 const Profile = () => {
   useUserAuth();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong> PLease try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Profile">
-      <div className="my-5 mx-auto"></div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-col-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={dashboardData?.totalBalance || 0}
+            color="bg-primary"
+          />
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={dashboardData?.totalIncome || 0}
+            color="bg-orange-500"
+          />
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={dashboardData?.totalExpense || 0}
+            color="bg-red-500"
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentIncome
+            transactions={dashboardData?.last60DaysIncome?.transactions}
+            onSeeMore={() => navigate("/income")}
+          />
+
+          <ExpenseTransactions
+            transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+            onSeeMore={() => navigate("/expense")}
+          />
+        </div>
+
+        <div className="card mt-6">
+          <div className="flex items-center justify-between">
+            <div className="">
+              <h5 className="text-lg">Profile Overview</h5>
+              <p className="text-xs text-gray-400 mt-0.5">
+                Easily view and update your profile details to keep your
+                information accurate and up-to-date
+              </p>
+            </div>
+
+            <button className="add-btn">
+              <RxUpdate className="text-lg" />
+              Update Profile
+            </button>
+
+            
+          </div>
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
